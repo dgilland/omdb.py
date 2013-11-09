a4ef0c2da6d08c70802d981aa56c76959c8d54e5
Derrick Gilland <dgilland@gmail.com>
2013-11-08 21:28:21 -0500

    Add package __init__.py.

---
+++ omdb/__init__.py
@@ -0,0 +1,12 @@
+
+from .api import (
+    search,
+    get,
+    title,
+    Client
+)
+
+__version__ = '0.0.0'
+__author__ = 'Derrick Gilland <dgilland@gmail.com>'
+
+__all__ = ['search', 'get', 'title', 'request', 'Client']
