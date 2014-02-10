31a72eb931c2967579be0f40540bc5746e76bf5b
Derrick Gilland <dgilland@gmail.com>
2014-02-09 23:50:09 -0500

    Release v0.1.1.

--- omdb/__init__.py
+++ omdb/__init__.py
@@ -8,6 +8,6 @@ from .api import (
     Client
 )
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Derrick Gilland <dgilland@gmail.com>'
 __all__ = ['set_default', 'search', 'get', 'title', 'imdbid', 'Client']
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='omdb',
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper for OMDb API: http://www.omdbapi.com/',
     long_description=__doc__,
     author='Derrick Gilland',
