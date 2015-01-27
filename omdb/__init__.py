0e938461bdce98b7820e815a9d779a803a48ff95
Derrick Gilland <dgilland@gmail.com>
2015-01-27 10:32:42 -0500

    Add metadata to main module.

--- omdb/__init__.py
+++ omdb/__init__.py
@@ -1,15 +1,25 @@
 """OMDb API library.
 """
 
+from .__meta__ import (
+    __title__,
+    __summary__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__,
+)
+
 from .api import (
-    set_default,
+    Client,
+    get,
+    imdbid,
+    request,
     search,
     search_movie,
     search_episode,
     search_series,
-    get,
+    set_default,
     title,
-    imdbid,
-    request,
-    Client
 )
